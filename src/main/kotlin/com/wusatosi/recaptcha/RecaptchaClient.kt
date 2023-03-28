d1cdcf74b447d6a9e0084bddb965e8be51ae8d03
wusatosi <26424577+wusatosi@users.noreply.github.com>
2023-03-28 14:37:28 -0400

    Working on Documentation

--- src/main/kotlin/com/wusatosi/recaptcha/RecaptchaClient.kt
+++ src/main/kotlin/com/wusatosi/recaptcha/RecaptchaClient.kt
@@ -7,11 +7,32 @@ import com.wusatosi.recaptcha.v3.RecaptchaV3Client
 
 interface RecaptchaClient {
 
+    /**
+     * Verify a given recapthca token.
+     * Note: this is a suspend function
+     *
+     * Example: {@sample
+     *   val token = tokenFromUser()
+     *   if (client.verify(token))
+     *      return "Yes, you are welcome!"
+     *   else
+     *      return "No, beep boop!!!"
+     * }
+     *
+     * @param token The given recapthca token
+     * @return If the token is valid
+     */
     @Throws(RecaptchaError::class)
     suspend fun verify(token: String): Boolean
 
     companion object {
 
+        /**
+         * Create a universal client for token verification.
+         * @param secretKey Secret key for cpathca verification (aka site key),
+         *      you should be able to find this in your recapthca console.
+         * @param defaultScoreThreshold If the client is internally v3 where Google returns a
+         */
         fun createUniversal(
             secretKey: String,
             defaultScoreThreshold: Double = 0.5,
