19ea347b45471403df21a0c875738123846a7364
wusatosi <26424577+wusatosi@users.noreply.github.com>
2023-03-30 17:17:52 -0400

    Remove Universal client

--- src/main/kotlin/com/wusatosi/recaptcha/RecaptchaClient.kt
+++ src/main/kotlin/com/wusatosi/recaptcha/RecaptchaClient.kt
@@ -1,7 +1,5 @@
 package com.wusatosi.recaptcha
 
-import com.wusatosi.recaptcha.internal.UniversalRecaptchaClientImpl
-import com.wusatosi.recaptcha.internal.likelyValidRecaptchaParameter
 import io.ktor.client.*
 import io.ktor.client.engine.*
 import io.ktor.client.engine.cio.*
@@ -11,30 +9,6 @@ interface RecaptchaClient : Closeable {
 
     @Throws(RecaptchaError::class)
     suspend fun verify(token: String, remoteIp: String = ""): Boolean
-
-    companion object {
-
-        fun createUniversal(
-            secretKey: String,
-            defaultScoreThreshold: Double = 0.5,
-            useRecaptchaDotNetEndPoint: Boolean = false,
-            engine: HttpClientEngine = CIO.create()
-        ): RecaptchaClient {
-            if (!likelyValidRecaptchaParameter(secretKey))
-                throw InvalidSiteKeyException
-
-            val config = RecaptchaV3Config()
-            config.scoreThreshold = defaultScoreThreshold
-            config.useAlternativeDomain = useRecaptchaDotNetEndPoint
-            config.engine = engine
-
-            return UniversalRecaptchaClientImpl(
-                secretKey,
-                config
-            )
-        }
-
-    }
 
 }
 
